f7d600b18b64db57c27aeb0eb273981412ba287d
aakash-s45 <tech2aks@gmail.com>
2023-03-03 11:58:39 +0530

    Initial Commit

---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by Aakash Solanki on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
